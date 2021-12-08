545b39df33433f0ad3a7ca2090550582cefd3d4e
Kevin <kjmoreno2@miners.utep.edu>
2021-12-07 23:25:25 -0700

    added jump tables to assembly file

--- project/dim_sequence_s.s
+++ project/dim_sequence_s.s
@@ -1,6 +1,13 @@
 	.arch msp430g2553
 	.p2align 1,0
 
+
+	.text
+jt:
+	.word case0
+	.word case1
+	.word case2
+	
 	.data
 dimState:	 .word 0
 
@@ -13,26 +20,22 @@ dimState:	 .word 0
 	.extern greenDim50
 	.global dim_sequence
 dim_sequence:
-	cmp #1, &dimState 	;switch statement check if dim state 0, 1, 2
-	jl case_0		;executes instructions according to value in dimstate
-	cmp #2, &dimState
-	jl case_1
-	cmp #3, &dimState
-	jl case_2
-case_0:
+	cmp #3, &dimState 	;switch statement check if dim state 0, 1, 2
+	jc out		;executes instructions according to value in dimstate
+	mov &dimState, r12
+	add r12, r12
+	mov jt(r12), r0
+case2:
 	call #redDim50
 	call #greenDim50
-	add #1, &dimState
-	jmp out
-case_1:
+	mov #0, &dimState
+case1:
 	call #redDim75
 	call #greenDim25
 	add #1, &dimState
-	jmp out
-case_2:
+case0:
 	call #redDim25
 	call #greenDim75
-	mov #0, &dimState
-	jmp out
+	add #1, &dimState
 out:
 	pop r0
